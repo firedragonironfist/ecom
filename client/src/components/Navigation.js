6452c346e58b7d3990632be269a3c584beffb66a
Yousuf <firedragonironfist998@gmail.com>
2023-07-15 09:21:10 +0530

    secured server properly

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -16,7 +16,7 @@ const Navbar = () => {
   );
 
   const checkout = async () => {
-    await fetch("https://ecom-ik60.onrender.com/checkout", {
+    await fetch("https://ecom-o62i.onrender.com/checkout", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
